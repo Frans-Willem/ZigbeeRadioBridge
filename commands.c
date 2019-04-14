683af1fdf5844f3dfcf1b7df95a8f9714234f698
Frans-Willem Hardijzer <fw@hardijzer.nl>
2019-04-14 16:26:49 +0200

    Implemented all NETSTACK_RADIO functions as serial commands.

--- commands.c
+++ commands.c
@@ -3,28 +3,101 @@
 #include "serial_protocol.h"
 #include "serialization.h"
 
+static void commands_handle_radio_get_value(size_t request_id,
+                                            radio_param_t param) {
+  radio_value_t value = 0;
+  radio_result_t result;
+  uint8_t serialized_response[4];
+  result = NETSTACK_RADIO.get_value(param, &value);
+  serialize_int(result, &serialized_response[0], 2);
+  serialize_int(result, &serialized_response[2], 2);
+  commands_send_ok(request_id, serialized_response,
+                   sizeof(serialized_response));
+}
+
+static void commands_handle_radio_set_value(size_t request_id,
+                                            const uint8_t *data,
+                                            size_t data_len) {
+  radio_param_t param;
+  radio_value_t value;
+  if (data_len < 4) {
+    commands_send_err(request_id, NULL, 0);
+    return;
+  }
+  param = deserialize_uint(&data[0], 2);
+  value = deserialize_int(&data[2], 2);
+  commands_send_ok_int(request_id, (int)NETSTACK_RADIO.set_value(param, value));
+}
+
+static void commands_handle_radio_get_object(size_t request_id,
+                                             const uint8_t *data,
+                                             size_t data_len) {
+  radio_param_t param;
+  uint8_t retval[64];
+  size_t expected_len;
+  if (data_len < 4) {
+    commands_send_err(request_id, NULL, 0);
+    return;
+  }
+  param = deserialize_uint(&data[0], 2);
+  expected_len = deserialize_uint(&data[2], 2);
+  if (2 + expected_len > sizeof(retval)) {
+    commands_send_err(request_id, NULL, 0);
+    return;
+  }
+  serialize_int(
+      (int)NETSTACK_RADIO.get_object(param, (void *)&retval[2], expected_len),
+      &retval[0], 2);
+  commands_send_ok(request_id, retval, 2 + expected_len);
+}
+
+static void commands_handle_radio_set_object(size_t request_id,
+                                             const uint8_t *data,
+                                             size_t data_len) {
+  radio_param_t param;
+  if (data_len < 2) {
+    commands_send_err(request_id, NULL, 0);
+    return;
+  }
+  param = deserialize_uint(data, data_len);
+  commands_send_ok_int(request_id, (int)NETSTACK_RADIO.set_object(
+                                       param, &data[2], data_len - 2));
+}
+
 void commands_handle_command(command_t cmd, size_t request_id,
                              const uint8_t *data, size_t data_len) {
   switch (cmd) {
   case Command_Request_Radio_Prepare:
-    commands_send_int_response(request_id, 0,
-                               NETSTACK_RADIO.prepare(data, data_len));
+    commands_send_ok_int(request_id, NETSTACK_RADIO.prepare(data, data_len));
     break;
   case Command_Request_Radio_Transmit:
-    commands_send_int_response(
-        request_id, RADIO_TX_OK,
-        NETSTACK_RADIO.transmit(deserialize_ushort(data, data_len)));
+    commands_send_ok_int(request_id, NETSTACK_RADIO.transmit(
+                                         deserialize_ushort(data, data_len)));
     break;
   case Command_Request_Radio_Send:
-    commands_send_int_response(request_id, RADIO_TX_OK,
-                               NETSTACK_RADIO.send(data, data_len));
+    commands_send_ok_int(request_id, NETSTACK_RADIO.send(data, data_len));
     break;
-    // TODO ChannelClear
+  case Command_Request_Radio_ChannelClear:
+    commands_send_ok_int(request_id, NETSTACK_RADIO.channel_clear());
+    break;
   case Command_Request_Radio_On:
-    commands_send_int_response(request_id, 1, NETSTACK_RADIO.on());
+    commands_send_ok_int(request_id, NETSTACK_RADIO.on());
     break;
   case Command_Request_Radio_Off:
-    commands_send_int_response(request_id, 1, NETSTACK_RADIO.off());
+    commands_send_ok_int(request_id, NETSTACK_RADIO.off());
+    break;
+  case Command_Request_Radio_GetValue:
+    commands_handle_radio_get_value(
+        request_id, (radio_param_t)deserialize_uint(data, data_len));
+    break;
+  case Command_Request_Radio_SetValue:
+    commands_handle_radio_set_value(request_id, data, data_len);
+    break;
+  case Command_Request_Radio_GetObject:
+    commands_handle_radio_get_object(request_id, data, data_len);
+    break;
+  case Command_Request_Radio_SetObject:
+    commands_handle_radio_set_object(request_id, data, data_len);
     break;
   default:
     commands_send_err(request_id, NULL, 0);
@@ -41,18 +114,18 @@ void commands_send_err(size_t request_id, const uint8_t *data,
   serial_send_frame((uint8_t)Command_Response_OK, request_id, data, data_len);
 }
 
-void commands_send_int_response(size_t request_id, int ok_value,
-                                int return_value) {
-  uint8_t return_value_serialized[2];
-  if (return_value == ok_value) {
-    commands_send_ok(request_id, NULL, 0);
-  } else {
-    serialize_int(return_value, return_value_serialized,
-                  sizeof(return_value_serialized));
-    commands_send_err(request_id, return_value_serialized,
-                      sizeof(return_value_serialized));
-  }
+void commands_send_ok_int(size_t request_id, int data) {
+  uint8_t data_serialized[2];
+  serialize_int(data, data_serialized, sizeof(data_serialized));
+  commands_send_ok(request_id, data_serialized, sizeof(data_serialized));
 }
+
+void commands_send_err_int(size_t request_id, int data) {
+  uint8_t data_serialized[2];
+  serialize_int(data, data_serialized, sizeof(data_serialized));
+  commands_send_err(request_id, data_serialized, sizeof(data_serialized));
+}
+
 void commands_send_event_on_packet(const uint8_t *data, size_t data_len,
                                    uint8_t rssi, uint8_t link_quality) {
   uint8_t postfix[2] = {rssi, link_quality};
--- commands.h
+++ commands.h
@@ -15,6 +15,11 @@ typedef enum {
   Command_Request_Radio_SetValue,
   Command_Request_Radio_GetObject,
   Command_Request_Radio_SetObject,
+  /**
+   * These are sent from Dongle -> PC
+   * Err is usually a protocol error, while OK may still contain a non-OK return
+   * value.
+   */
   Command_Response_OK = 0x80,
   Command_Response_Err,
   Command_Event_Radio_OnPacket = 0xC0
@@ -23,9 +28,10 @@ typedef enum {
 void commands_handle_command(command_t cmd, size_t request_id,
                              const uint8_t *data, size_t data_len);
 void commands_send_ok(size_t request_id, const uint8_t *data, size_t data_len);
+void commands_send_ok_int(size_t request_id, int data);
 void commands_send_err(size_t request_id, const uint8_t *data, size_t data_len);
-void commands_send_int_response(size_t request_id, int ok_value,
-                                int return_value);
-void commands_send_event_on_packet(const uint8_t *packet, size_t packet_len, uint8_t rssi, uint8_t link_quality);
+void commands_send_err_int(size_t request_id, int data);
+void commands_send_event_on_packet(const uint8_t *packet, size_t packet_len,
+                                   uint8_t rssi, uint8_t link_quality);
 #endif // _COMMANDS_H_
 
